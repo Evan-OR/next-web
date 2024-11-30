00bf93c848fecdd7117657f7c024c93d67ca4bd9
18EOR <42158152+18EOR@users.noreply.github.com>
2024-11-30 01:04:04 +0000

    updated nav, video player and added absolute paths

--- app/components/navigation/NavLinkButton.tsx
+++ app/components/navigation/NavLinkButton.tsx
@@ -1,45 +1,39 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { Button, Link, styled, useTheme } from '@mui/material';
-import { usePathname } from 'next/navigation';
+import { Button, Link, styled } from '@mui/material';
 
 const NavButton = styled(Button)(({ theme }) => ({
-    '&:hover': {
-        color: theme.palette.primary.main,
-        backgroundColor: 'transparent',
-    },
+  '&:hover': {
+    color: theme.palette.primary.main,
+    backgroundColor: 'transparent',
+  },
 }));
 
 const NavLink = styled(Link)({
-    color: 'inherit',
-    textDecoration: 'none',
+  color: 'inherit',
+  textDecoration: 'none',
 });
 
 type NavLinkButtonProps = {
-    href: string;
-    isNavButton: boolean;
-    text?: string;
-    children?: ReactNode;
+  href: string;
+  isNavButton: boolean;
+  text?: string;
+  children?: ReactNode;
 };
 
 const NavLinkButton = ({ href, text, isNavButton, children }: NavLinkButtonProps) => {
-    const pathname = usePathname();
-    const theme = useTheme();
-
-    // Checking if button is active and not the main logo
-    const color = href === pathname && text ? theme.palette.primary.main : 'inherit';
-    return (
-        <NavLink href={href} style={{ color: color }}>
-            {isNavButton ? (
-                <NavButton variant="text" color="inherit" disableRipple>
-                    {text}
-                </NavButton>
-            ) : (
-                children
-            )}
-        </NavLink>
-    );
+  return (
+    <NavLink href={href}>
+      {isNavButton ? (
+        <NavButton variant="text" color="inherit" disableRipple>
+          {text}
+        </NavButton>
+      ) : (
+        children
+      )}
+    </NavLink>
+  );
 };
 
 export default NavLinkButton;
